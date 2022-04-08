3b905f09d4dca9aa540cf592664c3d7209b853d6
llewelyn-williams <llewelyn.williams@googlemail.com>
2022-04-08 11:21:16 +0000

    feat: Customise volunteers admin

--- volunteers/admin.py
+++ volunteers/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Volunteer
 
+class VolunteerAdmin(admin.ModelAdmin):
+    list_display = (
+        'forename',
+        'surname',
+        'image',
+    )
 
 # Register your models here.
-admin.site.register(Volunteer)
+admin.site.register(Volunteer, VolunteerAdmin)
