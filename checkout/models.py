145c1d64773d404dd6b0a92f0ef1153633996ed1
llewelyn-williams <llewelyn.williams@googlemail.com>
2022-04-09 14:03:09 +0000

    feat: Create checkout app and model

---
+++ checkout/models.py
@@ -0,0 +1,44 @@
+""" Checkout Models """
+import uuid
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+
+
+class Order(models.Model):
+    """ The Order Model with details for Stripe payments
+    and details about the order. """
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    country = models.CharField(max_length=40, null=False, blank=False)
+    talking_topics = models.TextField(null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    order_total = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0
+        )
+
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
