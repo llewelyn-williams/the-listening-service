49ab996c2c62d4d2e289a21c77f11ea6c4226887
llewelyn-williams <llewelyn.williams@googlemail.com>
2022-04-09 14:45:47 +0000

    feat: Add checkout admin and form

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
-
+from .models import Order
 # Register your models here.
+
+
+class OrderAdmin(admin.ModelAdmin):
+    readonly_fields = ('order_number', 'date',
+                       'order_total',
+                       )
+
+    fields = ('order_number', 'full_name',
+              'email', 'phone_number',
+              'street_address1', 'street_address2',
+              'town_or_city', 'county',
+              'postcode', 'country',
+              'date', 'order_total',)
+
+    list_display = ('order_number', 'date',
+                    'full_name', 'order_total',
+                    )
+
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
