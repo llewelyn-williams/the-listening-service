101a634f5c68a16f9bde1040b13ffc53f9c73550
llewelyn-williams <llewelyn.williams@googlemail.com>
2022-04-10 11:16:17 +0000

    feat: Add basic checkout functionality with Stripe
    Currently the amount is set directly in the view but ultimately this
    should be obtained from a combination of a set value (minimum amount)
    and user input (additional tip/donation).

--- checkout/views.py
+++ checkout/views.py
@@ -1,15 +1,36 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+import stripe
 
 
 def checkout(request):
+
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+    stripe_total = 5000
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print(intent)
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51KhfwzBX1dPqJdtCcXX5ImiYoa9nRV8lJy8DJqm8kuSS4ZtTbaxzEruqX1MRbEl0tVE7WAl4bzjOrCObRHR9hOm000vjkaR9dv',
-        'client_secret': 'test_client_secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
