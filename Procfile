21290535ba17859d8e1abee4cd8d9ffee4e550d7
llewelyn-williams <llewelyn.williams@googlemail.com>
2022-04-11 18:42:11 +0000

    ops: Deploy to Heroku
    Installing gnuicorn, adding Procfile, temporarily setting
    DISABLE_COLLECTSTATIC on Heroku app.
    Listing Allowed Hosts in settings.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn the_listening_service.wsgi:application
--- the_listening_service/settings.py
+++ the_listening_service/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-t7h-w4y40$%s3%$bgc2uv!$^09fvbb_c&vcvmi+-1sl8p-pgsw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['the-listening-service.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,12 +120,17 @@ WSGI_APPLICATION = 'the_listening_service.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
