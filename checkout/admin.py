56b8c17099150ccabdb99275bde27a75995f6af8
llewelyn-williams <llewelyn.williams@googlemail.com>
2022-04-11 08:58:14 +0000

    feat: Add booking history and saving of address
    Order history now shows in the users profile. The 'save address info'
    option when placing a booking now also actually saves that info to their
    profile and it is used when placing bookings when logged in.

--- checkout/admin.py
+++ checkout/admin.py
@@ -8,7 +8,7 @@ class OrderAdmin(admin.ModelAdmin):
                        'order_total', 'stripe_pid'
                        )
 
-    fields = ('order_number', 'full_name',
+    fields = ('order_number', 'user_profile', 'full_name',
               'email', 'phone_number',
               'street_address1', 'street_address2',
               'town_or_city', 'county',
