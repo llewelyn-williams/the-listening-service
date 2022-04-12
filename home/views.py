fffc09fe382adfca191448f8b736aa9d6ad09769
llewelyn-williams <llewelyn.williams@googlemail.com>
2022-04-12 12:34:28 +0000

    feat: Add about page

--- home/views.py
+++ home/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
     """ The view for the index page. """
     return render(request, "home/index.html")
+
+
+def about(request):
+    """ The view for the about page. """
+    return render(request, "home/about.html")
